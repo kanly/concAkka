b7a2f7030c38d7957fdc31171dff962643558aa9
Paolo Mazzoncini <p.mazzoncini@gmail.com>
2013-05-08 14:58:05 +0200

    Use EventSys in EventSourceSpec

--- src/test/scala/airplane/TestEventSource.scala
+++ src/test/scala/airplane/TestEventSource.scala
@@ -1,33 +1,34 @@
 package airplane
 
-import akka.testkit.{TestActorRef, TestKit}
-import akka.actor.{Actor, ActorSystem}
-import org.scalatest.{BeforeAndAfterAll, WordSpec}
+import akka.testkit.TestActorRef
+import akka.actor.Actor
+import org.scalatest.{ParallelTestExecution, WordSpec}
 import org.scalatest.matchers.MustMatchers
 import airplane.ProductionEventSource.{UnregisterListener, RegisterListener}
+import helper.ActorSys
 
-class TestEventSource extends Actor with ProductionEventSource{
+class TestEventSource extends Actor with ProductionEventSource {
   def receive = eventSourceReceiver
 }
 
-class EventSourceSpec extends TestKit(ActorSystem("EventSourceSpec")) with WordSpec with MustMatchers with BeforeAndAfterAll {
-  override def afterAll(){system.shutdown()}
+class EventSourceSpec extends WordSpec with MustMatchers with ParallelTestExecution {
+
 
   "Event Source" should {
-    "allow us to register a listener" in {
+    "allow us to register a listener" in new ActorSys {
       val real = TestActorRef[TestEventSource].underlyingActor
       real.receive(RegisterListener(testActor))
-      real.listeners must contain (testActor)
+      real.listeners must contain(testActor)
     }
 
-    "allow us to unregister a listener" in {
+    "allow us to unregister a listener" in new ActorSys {
       val real = TestActorRef[TestEventSource].underlyingActor
       real.receive(RegisterListener(testActor))
       real.receive(UnregisterListener(testActor))
-      real.listeners.size must be (0)
+      real.listeners.size must be(0)
     }
 
-    "send the event to our test actor" in {
+    "send the event to our test actor" in new ActorSys {
       val testA = TestActorRef[TestEventSource]
       testA ! RegisterListener(testActor)
       testA.underlyingActor.sendEvent("Fibonacci")
