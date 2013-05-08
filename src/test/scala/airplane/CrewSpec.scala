5fe3826901b3c38fede25b5a7f39ab016ee5facb
Paolo Mazzoncini <p.mazzoncini@gmail.com>
2013-05-08 18:28:24 +0200

    Add LeadFlightAttendant test

--- src/test/scala/airplane/CrewSpec.scala
+++ src/test/scala/airplane/CrewSpec.scala
@@ -2,10 +2,12 @@ package airplane
 
 import akka.testkit.{TestActorRef, ImplicitSender, TestKit}
 import akka.actor.{Props, ActorSystem}
-import org.scalatest.WordSpec
+import org.scalatest.{BeforeAndAfterAll, WordSpec}
 import org.scalatest.matchers.MustMatchers
 import airplane.FlightAttendant.{Drink, GetDrink}
 import com.typesafe.config.ConfigFactory
+import airplane.LeadFlightAttendant.{Attendant, GetFlightAttendant}
+import airplane.Altimeter.AltitudeUpdate
 
 object TestFlightAttendant {
   def apply() = new FlightAttendant with AttendantResponsiveness {
@@ -13,11 +15,41 @@ object TestFlightAttendant {
   }
 }
 
-class CrewSpec extends TestKit(ActorSystem("CrewSpec", ConfigFactory.parseString("akka.scheduler.tick-duration = 1ms"))) with ImplicitSender with WordSpec with MustMatchers {
+object TestLeadFlightAttendant {
+  def apply():LeadFlightAttendant = new LeadFlightAttendant with AttendantCreationPolicy {
+    override val numberOfAttendants = 1
+
+    override def createAttendant = TestFlightAttendant()
+  }
+}
+
+class CrewSpec extends TestKit(ActorSystem("CrewSpec", ConfigFactory.parseString("akka.scheduler.tick-duration = 1ms"))) with ImplicitSender with WordSpec with MustMatchers  with BeforeAndAfterAll {
+
+  override def afterAll() {
+    system.shutdown()
+  }
 
   "FlighAttendant" should {
     "get a drink when asked" in {
       val act = TestActorRef(Props(TestFlightAttendant()))
+      act ! GetDrink("Cola")
+      expectMsg(Drink("Cola"))
+    }
+  }
+
+  "LeadFlighAttendant" should {
+    import airplane.FlightAttendant
+
+    "give an attendant when asked" in {
+      val act = TestActorRef(Props(TestLeadFlightAttendant()))
+      act ! GetFlightAttendant
+      fishForMessage() {
+        case Attendant(_) => true
+      }
+    }
+
+    "forward message to an attendant" in {
+      val act = TestActorRef(Props(TestLeadFlightAttendant()))
       act ! GetDrink("Cola")
       expectMsg(Drink("Cola"))
     }
