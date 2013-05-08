60b1a286c2d0c5b4cea3089ebdf54ae2262b882c
Paolo Mazzoncini <p.mazzoncini@gmail.com>
2013-05-08 17:31:01 +0200

    LeadAttendant actor

--- src/main/scala/airplane/Crew.scala
+++ src/main/scala/airplane/Crew.scala
@@ -3,8 +3,9 @@ package airplane
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.Random
 import scala.concurrent.duration._
-import akka.actor.Actor
+import akka.actor.{Props, ActorRef, Actor}
 import airplane.FlightAttendant.{Drink, GetDrink}
+import airplane.LeadFlightAttendant.{GetFlightAttendant, Attendant}
 
 trait AttendantResponsiveness {
   val maxResponseTimeMS: Int
@@ -24,11 +25,68 @@ object FlightAttendant {
 
 }
 
-
 class FlightAttendant extends Actor {
   this: AttendantResponsiveness =>
 
   def receive = {
     case GetDrink(drinkName) => context.system.scheduler.scheduleOnce(responseDuration, sender, Drink(drinkName))
+  }
+}
+
+trait AttendantCreationPolicy {
+  val numberOfAttendants = 8
+
+  def createAttendant = FlightAttendant()
+}
+
+trait LeadFlightAttendantProvider {
+  def newFlightAttendant = LeadFlightAttendant()
+}
+
+object LeadFlightAttendant {
+
+  case object GetFlightAttendant
+
+  case class Attendant(a: ActorRef)
+
+  def apply() = new LeadFlightAttendant with AttendantCreationPolicy
+}
+
+class LeadFlightAttendant extends Actor {
+  this: AttendantCreationPolicy =>
+
+  override def preStart() {
+    import scala.collection.JavaConverters._
+
+    val attendantNames = context.system.settings.config.getStringList("airplane.flightcrew.attendantNames").asScala
+    attendantNames take numberOfAttendants foreach {
+      i => context.actorOf(Props(createAttendant), i)
+    }
+  }
+
+  def randomAttendant(): ActorRef = {
+    context.children.take(
+      scala.util.Random.nextInt(numberOfAttendants) + 1).last
+  }
+
+  def receive = {
+    case GetFlightAttendant =>
+      sender ! Attendant(randomAttendant())
+    case m =>
+      randomAttendant() forward m
+  }
+}
+
+
+
+
+object FlightAttendantPathChecker {
+  def main(args: Array[String]) {
+    val system = akka.actor.ActorSystem("PlaneSimulation")
+    val lead = system.actorOf(Props(
+      new LeadFlightAttendant with AttendantCreationPolicy),
+      "LeadFlightAttendant")
+    Thread.sleep(2000)
+    system.shutdown()
   }
 }
