5efff5261218aaec9092d5b50f7b8195863c75e7
Paolo Mazzoncini <p.mazzoncini@gmail.com>
2013-05-22 11:08:35 +0200

    Make FlightAttendant stateful using become method to change behaviour at runtime

--- src/main/scala/airplane/Attendants.scala
+++ src/main/scala/airplane/Attendants.scala
@@ -2,10 +2,15 @@ package airplane
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.Random
-import akka.actor.{Props, ActorRef, Actor}
+import akka.actor.{Cancellable, Props, ActorRef, Actor}
 import scala.concurrent.duration._
-import airplane.FlightAttendant.{GetDrink, Drink}
-import airplane.LeadFlightAttendant.{GetFlightAttendant, Attendant}
+import airplane.FlightAttendant._
+import airplane.LeadFlightAttendant.GetFlightAttendant
+import airplane.FlightAttendant.Assist
+import airplane.FlightAttendant.GetDrink
+import airplane.FlightAttendant.Drink
+import scala.Some
+import airplane.LeadFlightAttendant.Attendant
 
 trait AttendantResponsiveness {
   val maxResponseTimeMS: Int
@@ -19,6 +24,14 @@ object FlightAttendant {
 
   case class Drink(drinkName: String)
 
+  case class Assist(passenger: ActorRef)
+
+  case object Busy_?
+
+  case object Yes
+
+  case object No
+
   def apply() = new FlightAttendant with AttendantResponsiveness {
     val maxResponseTimeMS = 300000
   }
@@ -28,9 +41,55 @@ object FlightAttendant {
 class FlightAttendant extends Actor {
   this: AttendantResponsiveness =>
 
-  def receive = {
-    case GetDrink(drinkName) => context.system.scheduler.scheduleOnce(responseDuration, sender, Drink(drinkName))
+  case class DeliverDrink(drink: Drink)
+
+  var pendingDelivery: Option[Cancellable] = None
+
+  def scheduleDelivery(drinkname: String): Cancellable = {
+    context.system.scheduler.scheduleOnce(responseDuration, self, DeliverDrink(Drink(drinkname)))
   }
+
+  def assistInjuredPassenger: Receive = {
+    case Assist(passenger) =>
+
+      pendingDelivery foreach {
+        _.cancel()
+      }
+      pendingDelivery = None
+      passenger ! Drink("Magic Healing Potion")
+  }
+
+  def handleDrinkRequests: Receive = {
+    case GetDrink(drinkname) =>
+      pendingDelivery = Some(scheduleDelivery(drinkname))
+
+      context.become(assistInjuredPassenger orElse
+        handleSpecificPerson(sender))
+    case Busy_? =>
+      sender ! No
+  }
+
+  def handleSpecificPerson(person: ActorRef): Receive = {
+    case GetDrink(drinkname) if sender == person =>
+      pendingDelivery foreach {
+        _.cancel()
+      }
+      pendingDelivery = Some(scheduleDelivery(drinkname))
+
+    case DeliverDrink(drink) =>
+      person ! drink
+      pendingDelivery = None
+
+      context.become(assistInjuredPassenger orElse handleDrinkRequests)
+
+    case m: GetDrink =>
+      context.parent forward m
+    case Busy_? =>
+      sender ! Yes
+  }
+
+  def receive = assistInjuredPassenger orElse handleDrinkRequests
+
 }
 
 trait AttendantCreationPolicy {
