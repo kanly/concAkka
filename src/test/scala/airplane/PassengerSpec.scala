04ffc12e82eeeadef4dc732dd0fcbabb0402fc51
Paolo Mazzoncini <p.mazzoncini@gmail.com>
2013-10-15 15:11:34 +0200

    SectionSpecificRouting spec

--- src/test/scala/airplane/PassengerSpec.scala
+++ src/test/scala/airplane/PassengerSpec.scala
@@ -1,13 +1,14 @@
 package airplane
 
 import scala.concurrent.duration._
-import akka.testkit.{TestProbe, ImplicitSender, TestKit}
+import akka.testkit.{ExtractRoute, TestProbe, ImplicitSender, TestKit}
 import akka.actor.{Actor, Props, ActorRef, ActorSystem}
 import org.scalatest.{BeforeAndAfterAll, WordSpec}
 import akka.event.Logging.Info
 import airplane.Passenger.FastenSeatbelts
 import org.scalatest.matchers.MustMatchers
 import com.typesafe.config.ConfigFactory
+import akka.routing.RouterConfig
 
 class PassengerSpec extends TestKit(ActorSystem()) with ImplicitSender with WordSpec with MustMatchers {
 
@@ -102,3 +103,54 @@ with ImplicitSender with WordSpec with BeforeAndAfterAll with MustMatchers {
     }
   }
 }
+
+
+class TestRoutee extends Actor {
+  def receive = {
+    case m => sender ! m
+  }
+}
+
+class RouterRelay extends Actor {
+  def receive = {
+    case _ =>
+  }
+}
+
+class SectionSpecificAttendantRouterSpec extends TestKit(ActorSystem("SectionSpecificAttendantRouterSpec")) with ImplicitSender with WordSpec with BeforeAndAfterAll with MustMatchers {
+
+  override def afterAll() {
+    system.shutdown()
+  }
+
+  def newRouter(): RouterConfig = new SectionSpecificAttendantRouter with FlightAttendantProvider {
+    override def newFlightAttendant() = new TestRoutee
+  }
+
+  def relayWithRow(row: Int) = system.actorOf(Props[RouterRelay], s"Someone-$row-C")
+
+  val passengers = (1 to 25).map(relayWithRow)
+  "SectionSpecificAttendantRouter" should {
+    "route consistently" in {
+      val router = system.actorOf(Props[TestRoutee].withRouter(newRouter()))
+      val route = ExtractRoute(router)
+      val routeA = passengers.slice(0, 10).map {
+        p => route(p, "Hi")
+      }.flatten
+      routeA.tail.forall {
+        _.recipient == routeA.head.recipient
+      } must be(true)
+      val routeAB = passengers.slice(9, 11).map {
+        p => route(p, "Hi")
+      }.flatten
+      routeAB.head must not be (routeAB.tail.head)
+      val routeB = passengers.slice(10, 20).map {
+        p => route(p, "Hi")
+      }.flatten
+      routeB.tail.forall {
+        _.recipient == routeB.head.recipient
+      } must be(true)
+    }
+  }
+}
+
